deb98d0451db4de6ed1d3abd335db1a1656db660
s.kusuhara <s.kusuhara@trustridge.jp>
2020-03-08 19:13:32 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <!-- <div class=""></div> -->
   </div>
 </template>
 
@@ -9,10 +8,7 @@
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  //...
 }
 </script>
 
---
+++ src/components/store.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+//ストアの定義
+const store = new Vuex.Store({
+    //...
+})
+
+//ストアをエクスポート
+export default store
+
